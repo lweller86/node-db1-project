141cca0ae0692e0908acb79065a49080f5449ba0
lweller86 <112785242+lweller86@users.noreply.github.com>
2023-05-10 12:00:02 -0400

    get function working correctly

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,22 +1,49 @@
 const router = require('express').Router()
+const md = require('./accounts-middleware')
+const Account = require('./accounts-model')
 
-router.get('/', (req, res, next) => {
- 
+router.get('/', async (req, res, next) => {
+ try{
+  const accounts = await Account.getAll()
+  res.json(accounts)
+ } catch (err) {
+  next(err)
+ }
 })
 
-router.get('/:id', (req, res, next) => {
+router.get(
+  '/:id',
+   md.checkAccountId,
+   async (req, res, next) => {
+    try{
+      const account = await Account.getById(req.params.id)
+      res.json(account)
+     } catch (err) {
+      next(err)
+     }
+})
+
+router.post(
+  '/', 
+  md.checkAccountPayload, 
+  md.checkAccountNameUnique, 
+  (req, res, next) => {
   // DO YOUR MAGIC
 })
 
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.put('/:id', (req, res, next) => {
+router.put(
+  '/:id', 
+  md.checkAccountId, 
+  md.checkAccountNameUnique, 
+  md.checkAccountPayload,  
+  (req, res, next) => {
   // DO YOUR MAGIC
 });
 
-router.delete('/:id', (req, res, next) => {
+router.delete(
+  '/:id', 
+  md.checkAccountId, 
+  (req, res, next) => {
   // DO YOUR MAGIC
 })
 
